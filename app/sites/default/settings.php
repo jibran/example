a1911d951b2e0a6ffc89c6cc0d04ba52a0d20218
Nick Schuch <nick@myschuch.com>
2019-05-02 14:39:26 +1000

    Local env updates

--- app/sites/default/settings.php
+++ app/sites/default/settings.php
@@ -21,7 +21,7 @@ $databases['default']['default'] = array(
   'database' => skpr_config('mysql.default.database') ?: 'local',
   'username' => skpr_config('mysql.default.username') ?: 'drupal',
   'password' => skpr_config('mysql.default.password') ?: 'drupal',
-  'host' => skpr_config('mysql.default.hostname') ?: '127.0.0.1',
+  'host' => skpr_config('mysql.default.hostname') ?: 'mysql',
 );
 
 $settings['hash_salt'] = skpr_config('salt') ?: '';
@@ -29,11 +29,11 @@ $settings['hash_salt'] = skpr_config('salt') ?: '';
 $config_directories[CONFIG_SYNC_DIRECTORY] = __DIR__ . '/../../../config-export';
 
 $config['cron_safe_threshold'] = '0';
-$settings['file_public_path'] = skpr_config('mount.public') ?: 'sites/default/files';
+$settings['file_public_path'] = 'sites/default/files';
 $config['system.file']['path']['temporary'] = skpr_config('mount.temporary') ?: '/tmp';
 $settings['file_private_path'] = skpr_config('mount.private') ?: 'sites/default/files/private';
 
-$settings['install_profile'] = 'standard';
+$settings['install_profile'] = 'demo_umami';
 
 $settings['trusted_host_patterns'][] = '^127\.0\.0\.1$';
 $settings['trusted_host_patterns'][] = '^localhost$';
